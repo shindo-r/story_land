830a37650f2c6b26f273c6637ba4f25180f0470c
shindoryohei <m5111123@gmail.com>
2013-01-28 18:17:46 +0900

    new rails project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+StoryLand::Application.config.secret_token = 'c8420fe7eb7e3fe2d7f6880592b36b18d1dc82a9fb2f9a555bb81999223b3afc4bf77f34d9e42c4e479db40e73abcd36ae5fea04841b57a906de5f93bc62486c'
